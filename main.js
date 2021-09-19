e03c78456de78615400ac9fdf25cf85dd6ed8fd4
Ankit Kumain <kumankit10@gmail.com>
2021-09-19 11:19:10 +0530

    refactor: show cash given input onclick

--- main.js
+++ main.js
@@ -4,26 +4,39 @@ var cashGiven = document.querySelector("#cash-given")
 
 var checkButton = document.querySelector("#btn-check")
 
+var nextButton = document.querySelector("#btn-next")
+
+var elementsHidden = document.querySelector(".elements-hidden")
+
 var message = document.querySelector(".message")
 
 var noOfNotes = document.querySelectorAll(".no-of-notes")
+
 var denominations = [2000,500,100,20,10,5,1]
 
+elementsHidden.style.display = "none"
+
+function nextButtonEventHandler(){
+    if(billAmount.value>0){
+        elementsHidden.style.display = "block"
+        message.style.display = "none"
+    }else{
+        message.style.display = "block"
+        billAmountValidation();
+    }
+}
+
 function checkButtonEventHandler(){
-    message.style.display = "none"
 
     if(billAmount.value>0){
-        
         if(Number(cashGiven.value) >= Number(billAmount.value)){
+            message.style.display = "none"
             var amountLeft = cashGiven.value - billAmount.value
             calculateNoOfNotes(amountLeft)           
         }else{
             message.style.display = "block"
             cashGivenValidation()
         }
-    }else{
-        message.style.display = "block"
-        billAmountValidation();
     }
         
 }
@@ -42,12 +55,12 @@ function cashGivenValidation(){
 }
 
 function billAmountValidation(){
+
     if(isNaN(billAmount.value)===true){    
         message.innerText = "Amount should be a number"
     }else{
         message.innerText = "Bill amount should be greater than zero"
     }
-
 }
 
 function calculateNoOfNotes(amountLeft){
@@ -58,4 +71,7 @@ function calculateNoOfNotes(amountLeft){
     }
     
 }
+
+
 checkButton.addEventListener("click",checkButtonEventHandler)
+nextButton.addEventListener("click",nextButtonEventHandler)
