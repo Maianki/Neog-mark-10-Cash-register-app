d19b035e96681de28cb38b266d58394a6fad774b
Ankit Kumain <kumankit10@gmail.com>
2021-09-19 07:53:53 +0530

    feat: added feature to calculate minimum no of notes

---
+++ main.js
@@ -0,0 +1,61 @@
+var billAmount = document.querySelector("#bill-amount")
+
+var cashGiven = document.querySelector("#cash-given")
+
+var checkButton = document.querySelector("#btn-check")
+
+var message = document.querySelector(".message")
+
+var noOfNotes = document.querySelectorAll(".no-of-notes")
+var denominations = [2000,500,100,20,10,5,1]
+
+function checkButtonEventHandler(){
+    message.style.display = "none"
+
+    if(billAmount.value>0){
+        
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            var amountLeft = cashGiven.value - billAmount.value
+            calculateNoOfNotes(amountLeft)           
+        }else{
+            message.style.display = "block"
+            cashGivenValidation()
+        }
+    }else{
+        message.style.display = "block"
+        billAmountValidation();
+    }
+        
+}
+
+
+function cashGivenValidation(){
+
+    if(isNaN(cashGiven.value)===true){    
+        message.innerText = "Cash Given should be a number and greater than bill amount"
+    }else if(cashGiven.value<0){
+        message.innerText = "Cash given should be greater than zero and bill amount"
+    }else{
+        message.innerText = "Cash given should be greater than bill amount"
+    }
+
+}
+
+function billAmountValidation(){
+    if(isNaN(billAmount.value)===true){    
+        message.innerText = "Amount should be a number"
+    }else{
+        message.innerText = "Bill amount should be greater than zero"
+    }
+
+}
+
+function calculateNoOfNotes(amountLeft){
+    for(var i=0; i<denominations.length;i++){
+        var totalNotes = parseInt(amountLeft/denominations[i])
+        amountLeft = amountLeft % denominations[i]
+        noOfNotes[i].innerText = totalNotes
+    }
+    
+}
+checkButton.addEventListener("click",checkButtonEventHandler)
