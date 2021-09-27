ee166c1073447df81aff5adeff6cdc73ecb0682e
Ankit Kumain <kumankit10@gmail.com>
2021-09-28 01:31:38 +0530

    style: output message display

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ function billAmountValidation(){
 }
 
 function calculateNoOfNotes(amountLeft){
+    message.style.display = "block"
     message.innerText = "The total amount to be returned is "+ amountLeft
     for(var i=0; i<denominations.length;i++){
         var totalNotes = parseInt(amountLeft/denominations[i])
